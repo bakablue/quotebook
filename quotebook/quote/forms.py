7e7054755474a3e6b2ba5c86a05ec62040ce8b44
Mélanie 'bakablue' Godard <melanie.godard@gmail.com>
2015-10-26 10:09:25 +0100

    git: .gitignore modified, quoteform updated.

--- quotebook/quote/forms.py
+++ quotebook/quote/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 class QuoteForm(forms.Form):
     author  = forms.CharField(max_length=20)
-    context = forms.CharField(widget=forms.Textarea)
+    context = forms.CharField(widget=forms.Textarea, required=False)
     quote   = forms.CharField(widget=forms.Textarea)
 
 class UserForm(forms.Form):
